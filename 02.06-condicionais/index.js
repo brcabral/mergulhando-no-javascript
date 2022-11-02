d7ed708818b866a3055510e0ebca7adeef7d4045
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-01 23:19:44 -0300

    2.6. Condicionais

---
+++ 02.06-condicionais/index.js
@@ -0,0 +1,12 @@
+var number01 = 8;
+var number02 = '8';
+
+if (number01 == number02) {
+    console.log('Os valores são iguais');
+}
+
+if (number01 === number02) {
+    console.log('Os valores e o tipo são iguais')
+} else {
+    console.log('O valor e/ou o tipo são diferentes')
+}
