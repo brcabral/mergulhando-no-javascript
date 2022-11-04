1cf606dbaf7568650f843a26c4fe28cbd5d16cbd
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-03 22:38:40 -0300

    3.5. Declarando variáveis (let)

---
+++ 03.05-declarando-variaveis-let/script.js
@@ -0,0 +1,13 @@
+/**
+ * O let é usado para declarar variáveis, porém diferente do var
+ *   não é possível reatribuir o valor da variável (let)
+ * Diferente do var o let é delimitado por bloco.
+ */
+
+let age = 21
+// let age = 27
+
+if (age > 18) {
+    let age = 32
+    console.log(age)
+}
