b414344a7393a716059d9a140cea596c77512fae
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-03 21:58:12 -0300

    3.3. Entendendo o uso do var

---
+++ 03.03-entendendo-o-uso-do-var/script.js
@@ -0,0 +1,9 @@
+var age = 21
+
+if (age > 18) {
+    var test = 35
+}
+
+// É possível acessar a variável que foi definida dentro do if
+// Pois as variáveis definidas com var é delimitada por função e não por blocos
+console.log(test)
