9e3f140403718e64f7aedc4ca73485f5a1456fcb
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-09 23:13:28 -0300

    3.8. Criando arrow functions

---
+++ 03.08-arrow-functions/script.js
@@ -0,0 +1,8 @@
+const getRemainingYears_1 = (age) => 18 - age;
+
+const getRemainingYears_2 = (age) => {
+    return 18 - age
+}
+
+console.log('getRemainingYears_1: ' + getRemainingYears_1(15));
+console.log('getRemainingYears_2: ' + getRemainingYears_2(15));
