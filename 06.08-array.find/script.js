18ebb82832c126ce8c3cc8622475bac149b2141d
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-17 00:16:22 -0300

    6.8. Encontrando um registro específico em uma lista com Array.find

---
+++ 06.08-array.find/script.js
@@ -0,0 +1,17 @@
+const students = [
+    { name: 'Lucas',    grade: 8 },
+    { name: 'Mario',    grade: 2 },
+    { name: 'Jean',     grade: 10 },
+    { name: 'Rogerio',  grade: 6 },
+    { name: 'Marcos',   grade: 5 },
+    { name: 'Jean',     grade: 6 }
+  ]
+
+  const specificStudent = students.find(pupil => {
+    if (pupil.name === 'Jean') {
+        return true
+    }
+  })
+
+  console.log(specificStudent)
+  
