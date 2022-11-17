1d81923b29fbad8aadf0d89cfb75588f7e2c96ed
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-17 00:26:12 -0300

    6.9. Validando elementos de uma lista com Array.some e Array.every

---
+++ 06.09-array.some-array.every/script.js
@@ -0,0 +1,16 @@
+const guests = [
+    { name: 'Marcos',   age: 23 },
+    { name: 'Angelica', age: 27 },
+    { name: 'Luana',    age: 17 },
+    { name: 'Leticia',  age: 45 },
+    { name: 'Raphael',  age: 18 },
+]
+
+// existem maiores de idade na lista de convidados?
+const hasAdults = guests.some(guest => guest.age >= 18)
+
+// todos os convidados são maiores de idade?
+const everyoneIsAnAdult = guests.every(guest => guest.age >= 18)
+
+console.log(`has adults: ${hasAdults}`)
+console.log(`everyone is an adult: ${everyoneIsAnAdult}`)
