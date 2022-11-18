8248fa31b83468148e4dd3fd31d862c02c8b164d
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-18 16:06:29 -0300

    9.3. Type aliasing

---
+++ 09.03-type-aliasing/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const cpf1 = 123;
+const cpf2 = '123';
+const sex = "male";
---
+++ 09.03-type-aliasing/index.ts
@@ -0,0 +1,7 @@
+type Cpf = string | number
+type Sex = 'male' | 'female' | undefined
+
+const cpf1: Cpf = 123
+const cpf2: Cpf = '123'
+
+const sex: Sex = "male"
