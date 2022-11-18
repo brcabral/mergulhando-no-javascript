e00d106e129a719af3b4d11d374f5b91262ee75a
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2022-11-18 15:35:28 -0300

    9.2. Tipando variáveis

---
+++ 09.02-tipando-variaveis/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const personName = 'Mateus';
+const personAge = 29;
+const personAlive = true;
+const personContacts = [
+    'mateus@mail.com',
+    16992355046
+];
---
+++ 09.02-tipando-variaveis/index.ts
@@ -0,0 +1,8 @@
+const personName: string = 'Mateus'
+const personAge: number = 29
+const personAlive: boolean = true
+
+const personContacts: (string|number)[] = [
+    'mateus@mail.com',
+    16992355046
+]
